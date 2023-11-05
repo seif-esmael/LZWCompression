1bfb3053fcc3ad23e30e3eaf209dde6ed5d58030
ziadayman1794 <101672194+ziadayman1794@users.noreply.github.com>
2023-11-05 03:00:51 +0200

    implmenting the decompress function and write it to file

--- src/lzw/LZW.java
+++ src/lzw/LZW.java
@@ -9,10 +9,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -51,7 +48,6 @@ public class LZW {
             return null;
         }        
     }
-    //__________________________________________________________________________________________________________________
     private static String readFile(String filePath) throws IOException {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -62,7 +58,6 @@ public class LZW {
         }
         return content.toString();
     }
-    //__________________________________________________________________________________________________________________
     public static void saveCompressedToFile(List<Integer> compressed, String outputPath) {
         try {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
@@ -75,13 +70,62 @@ public class LZW {
             e.printStackTrace();
         }
     }
-    //__________________________________________________________________________________________________________________
-    public static void main(String[] args) {          
-        //String text = "ABAABABBAABAABAAAABABBBBBBBB";
-        String Ifile = "C:\\Users\\Seif\\Desktop\\Java\\LZW\\input.txt";
-        String Ofile = "compressed.txt";
-        List<Integer> compressed = compress(Ifile);
-        saveCompressedToFile(compressed, Ofile);
-        System.out.println("Compressed data saved to " + Ofile + " file");
-    }                       
+    public static void saveDecompressedToFile(String decompressed, String outputPath) {
+        try {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
+                writer.write(decompressed);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void decompress(){
+        try {
+            String inputToBeCompressed=readFile("compressed.txt");
+            String[] input=inputToBeCompressed.split(" ");
+            int[] inputInt=new int[input.length];
+            for (int i = 0; i < input.length; i++) {
+                inputInt[i]=Integer.parseInt(input[i]);
+            }
+            Map<Integer, String> dictionary = new HashMap<>();
+            StringBuilder output= new StringBuilder();
+            int startNumber = 128;
+            String lastOperationSymbols="";
+            for (int i = 0; i < input.length; i++) {
+                if(dictionary.containsKey(inputInt[i])){
+                    output.append(dictionary.get(inputInt[i]));
+                    dictionary.put( startNumber++,lastOperationSymbols+dictionary.get(inputInt[i]).charAt(0));
+                    lastOperationSymbols=dictionary.get(inputInt[i]);
+
+                }
+                else{
+                    if(i==0){
+                        output.append((char) inputInt[i]);
+                        lastOperationSymbols+=(char)inputInt[i];
+                    }
+                    else{
+                        if(inputInt[i]>=128){
+                            output.append(lastOperationSymbols+lastOperationSymbols.charAt(0));
+                            dictionary.put(startNumber++,lastOperationSymbols+lastOperationSymbols.charAt(0));
+                            lastOperationSymbols+=lastOperationSymbols.charAt(0);
+                        }
+                        else{
+                            output.append((char)inputInt[i]);
+                            dictionary.put(startNumber++,lastOperationSymbols+(char)inputInt[i]);
+                            lastOperationSymbols=""+(char)inputInt[i];
+                        }
+                    }
+                }
+            }
+            saveDecompressedToFile(output.toString(),"decompressed.txt");
+        } catch (IOException ex) {
+            Logger.getLogger(LZW.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    public static void main(String[] args) {
+
+
+    }
 }
